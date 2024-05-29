9c34ff9ad78b419bf2342476bf1c09b554cf192f
Говорухин Александр Дмитриевич <a.govorukhin@ase-ec.ru>
2024-05-29 19:20:17 +0300

    add admin user

---
+++ src/backend/backend.DataAccess/Migrations/20240529151548_AddAdminUser.cs
@@ -0,0 +1,34 @@
+﻿using backend.Core.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminUser : Migration
+    {
+        private static readonly string[] columns = new []{ "Key", "Login", "Password", "Role" };
+
+        private static readonly object[] values = new object[] { Guid.NewGuid(), "admin", BCrypt.Net.BCrypt.EnhancedHashPassword("admin"), UserRole.Admin.ToString() };
+
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users", 
+                columns: columns,
+                values: values);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users", 
+                keyColumn: columns.First(),
+                keyColumnType: "Guid",
+                keyValue: values.First());
+        }
+    }
+}
