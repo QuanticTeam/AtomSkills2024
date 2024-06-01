1bed06a6ad8380bbd0012a31bee5f165df7063a1
Петевотов Виктор Геворкович <v.petevotov@ase-ec.ru>
2024-06-01 17:40:55 +0300

    Request status notification proto

--- src/frontend/src/ui/pages/MainPage.tsx
+++ src/frontend/src/ui/pages/MainPage.tsx
@@ -1,15 +1,42 @@
 import { faker } from '@faker-js/faker'
-import { Button } from 'antd'
+import { Button, notification } from 'antd'
 import { ProtectedTemplate } from '../templates/ProtectedTemplate'
 import { http } from '../../http'
+import { AxiosError } from 'axios'
 
 export function MainPage() {
+  const [notificationApi, contextHolder] = notification.useNotification()
+
   return (
     <ProtectedTemplate title="Page title">
+      {contextHolder}
+
       <div>
         <p>
-          <Button onClick={() => http.post(`/Something/Test`)}>Backend test request</Button>
+          <Button
+            type="primary"
+            onClick={async () => {
+              try {
+                const result = await http.post(`/Something/Test`)
+
+                notificationApi.success({
+                  message: 'Request OK',
+                  description: result.data,
+                })
+              } catch (e) {
+                if (e instanceof AxiosError) {
+                  notificationApi.error({
+                    message: e.message,
+                    description: <code className="text-xs">{JSON.stringify(e.response)}</code>,
+                  })
+                }
+              }
+            }}
+          >
+            Backend test request
+          </Button>
         </p>
+        <br />
         <p>{faker.lorem.sentence(1000)}</p>
       </div>
     </ProtectedTemplate>
