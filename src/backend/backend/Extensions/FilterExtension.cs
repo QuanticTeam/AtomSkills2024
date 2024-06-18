c29de6115ec1e51b4c9d0808178b7bb589060487
Dmitry Tyurnikov <DimsFromDergachy@outlook.com>
2024-06-19 03:05:30 +0500

    Fix int32 filter

--- src/backend/backend/Extensions/FilterExtension.cs
+++ src/backend/backend/Extensions/FilterExtension.cs
@@ -12,27 +12,28 @@ public static class FilterExtension
         foreach (var filter in filters)
         {
             var columnName = filter.ColumnName.ToFirstLetterUpper();
-            
+
             var type = typeof(TSource).GetProperty(columnName);
-            
+
             if (type == null)
                 continue;
-            
+
             var typeExpression = type.PropertyType.Name switch
             {
                 "String" => StringFilter<TSource>(columnName, filter.Values.Single(), filter.FilterType),
                 "Int" => IntFilter<TSource>(columnName, int.Parse(filter.Values.Single()), filter.FilterType),
+                "Int32" => IntFilter<TSource>(columnName, int.Parse(filter.Values.Single()), filter.FilterType),
                 "Double" => DoubleFilter<TSource>(columnName, double.Parse(filter.Values.Single()), filter.FilterType),
                 "DateTime" => DateTimeFilter<TSource>(columnName, DateTime.Parse(filter.Values.Single()).ToUniversalTime(), filter.FilterType),
                 _ => throw new ArgumentOutOfRangeException(nameof(type), type, "Filter does not exist")
             };
-            
+
             expression = expression.AndAlso(typeExpression);
         }
 
         return expression;
     }
-    
+
     private static Expression<Func<TSource, bool>> AndAlso<TSource>(this Expression<Func<TSource, bool>> expr1, Expression<Func<TSource, bool>> expr2)
     {
         if (expr2 == ConstFilter<TSource>.True)
@@ -53,9 +54,9 @@ public static class FilterExtension
 
         return Expression.Lambda<Func<TSource, bool>>(Expression.AndAlso(left, right), parameter);
     }
-    
+
     private static Expression Replace(this Expression body, Expression one, Expression another) => new Replacer(one, another).Visit(body);
-    
+
     private class Replacer(Expression one, Expression another) : ExpressionVisitor
     {
         public override Expression Visit(Expression node)
@@ -63,50 +64,43 @@ public static class FilterExtension
             return node == one ? another : base.Visit(node);
         }
     }
-    
+
     private static class ConstFilter<TSource>
     {
         public static readonly Expression<Func<TSource, bool>> True = x => true;
     }
-    
+
     private static Expression<Func<TSource, bool>> StringFilter<TSource>(string propertyName, string value, FilterType type)
     {
         var parameter = Expression.Parameter(typeof(TSource), "x");
-        
+
         var property = Expression.Property(parameter, propertyName);
-        
+
         var constant = Expression.Constant(value);
-        
+
         var method = type switch
         {
             FilterType.Equals => typeof(string).GetMethod("Equals", new[] { typeof(string) }),
             FilterType.Contains => typeof(string).GetMethod("Contains", new[] { typeof(string) }),
             _ => throw new ArgumentOutOfRangeException(nameof(type), type, "Filter does not exist")
         };
-        
+
         var expression = Expression.Call(property, method!, constant);
-        
+
         return Expression.Lambda<Func<TSource, bool>>(expression, parameter);
     }
-    
+
     private static Expression<Func<TSource, bool>> IntFilter<TSource>(string propertyName, int value, FilterType type)
     {
         var parameter = Expression.Parameter(typeof(TSource), "x");
 
         var property = Expression.Property(parameter, propertyName);
 
-        if (type == FilterType.Contains)
-        {
-                var constant2 = Expression.Constant(value.ToString());
-                var method2 = typeof(string).GetMethod("Contains", new[] { typeof(string) });
-                var expression2 = Expression.Call(property, method2!, constant2);
-                return Expression.Lambda<Func<TSource, bool>>(expression2, parameter);
-        }
-
         var constant = Expression.Constant(value);
 
         BinaryExpression expression = type switch
         {
+            FilterType.Contains => Expression.Equal(property, constant),
             FilterType.Equals => Expression.Equal(property, constant),
             FilterType.GreaterThan => Expression.GreaterThan(property, constant),
             FilterType.LessThan => Expression.LessThan(property, constant),
@@ -115,25 +109,18 @@ public static class FilterExtension
 
         return Expression.Lambda<Func<TSource, bool>>(expression, parameter);
     }
-    
+
     private static Expression<Func<TSource, bool>> DoubleFilter<TSource>(string propertyName, double value, FilterType type)
     {
         var parameter = Expression.Parameter(typeof(TSource), "x");
-        
+
         var property = Expression.Property(parameter, propertyName);
 
-        if (type == FilterType.Contains)
-        {
-                var constant2 = Expression.Constant(value.ToString());
-                var method2 = typeof(string).GetMethod("Contains", new[] { typeof(string) });
-                var expression2 = Expression.Call(property, method2!, constant2);
-                return Expression.Lambda<Func<TSource, bool>>(expression2, parameter);
-        }
-
         var constant = Expression.Constant(value);
-        
+
         var expression = type switch
         {
+            FilterType.Contains => Expression.Equal(property, constant),
             FilterType.Equals => Expression.Equal(property, constant),
             FilterType.GreaterThan => Expression.GreaterThan(property, constant),
             FilterType.LessThan => Expression.LessThan(property, constant),
@@ -148,17 +135,10 @@ public static class FilterExtension
         var parameter = Expression.Parameter(typeof(TSource), "x");
         var property = Expression.Property(parameter, propertyName);
 
-        if (type == FilterType.Contains)
-        {
-                var constant2 = Expression.Constant(value.ToString());
-                var method2 = typeof(string).GetMethod("Contains", new[] { typeof(string) });
-                var expression2 = Expression.Call(property, method2!, constant2);
-                return Expression.Lambda<Func<TSource, bool>>(expression2, parameter);
-        }
-
         var constant = Expression.Constant(value);
         var expression = type switch
         {
+            FilterType.Contains => Expression.Equal(property, constant),
             FilterType.Equals => Expression.Equal(property, constant),
             FilterType.GreaterThan => Expression.GreaterThan(property, constant),
             FilterType.LessThan => Expression.LessThan(property, constant),
